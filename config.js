e5c87285be9a600f9ea0fcd3128dd4efb120a152
Mister-Kay <28190493+Mister-Kay@users.noreply.github.com>
2017-08-09 19:19:46 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+module.exports = {
+	0: { // Warrior
+		22: true // Backstab
+	},
+	2: { // Slayer
+		// I don't have a slayer to test, so not sure if this works.
+		// But it should work same as warrior, so I left it true.
+		6: true // Backstab
+	},
+	10: { // Brawler
+		20: true // Meat Grinder
+	},
+	11: { // Ninja
+		// I don't have a ninja to test, so not sure if this works.
+		// Change to true to enable.
+		7: false // Decoy
+	},
+	12: { // Valkyrie
+		// I don't have a valkyrie to test, so not sure if this works.
+		// Change to true to enable.
+		20: false // Backstab
+	}
+}
