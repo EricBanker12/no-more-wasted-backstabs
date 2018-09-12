8a1051235e869b5d4c3107081afa07133a37f259
Mister-Kay <private-information>
2018-09-11 19:05:09 -0500

    fix for patch 74

--- index.js
+++ index.js
@@ -9,16 +9,19 @@ module.exports = function noMoreWastedBackstabs(dispatch) {
     dispatch.hook('S_LOGIN', 10, event => { job = (event.templateId - 10101) % 100 })
 
     // block no-target C_START_TARGETED_SKILL
-    dispatch.hook('C_START_TARGETED_SKILL', 3, {order: -100}, event => {
+    dispatch.hook('C_START_TARGETED_SKILL', dispatch.base.majorPatchVersion >= 74 ? 6 : 5, {order: -100}, event => {
         // get skill used
-        let skill = event.skill - 0x4000000,
+        let skill = event.skill.id //event.skill - 0x4000000,
             skillBase = Math.floor(skill / 10000)
         // if class and skill are in config
         if(config[job] && config[job][skillBase]) {
             // if skill has no target
             if(event.targets[0].id.equals(0)) {
                 // block the skill usage
-                dispatch.toClient('S_CANNOT_START_SKILL', 1, {skill: event.skill})
+                Object.assign(event.skill, {type: 0, npc: false, huntingZoneId: 0, reserved: 0})
+                dispatch.toClient('S_CANNOT_START_SKILL', dispatch.base.majorPatchVersion >= 74 ? 6 : 5, {
+                    skill: event.skill
+                })
                 return false
             }
         }
